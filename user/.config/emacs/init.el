59d27b6ede78e47c29979182068f6cfc480ba500
Andrej Balaz <andrejbalaz001@gmail.com>
2023-05-03 13:21:01 +0200

    added more cnfgs

---
+++ user/.config/emacs/init.el
@@ -0,0 +1,14 @@
+(add-to-list 'default-frame-alist '(font . "Robot Mono-20" ))
+(set-face-attribute 'default t :font "Roboto Mono-20" )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(user-init-file "/home/balaz/.config/emacs/init.el" t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
