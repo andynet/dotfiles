64ac23cfbda6738bf9e6904487e1bb555d3c8edb
Andrej Balaz <andrejbalaz001@gmail.com>
2023-06-01 23:55:40 +0200

    added doom emacs

--- user/.config/doom/custom.el
+++ user/.config/doom/custom.el
@@ -1,11 +1,10 @@
-(add-to-list 'default-frame-alist '(font . "Robot Mono-20" ))
-(set-face-attribute 'default t :font "Roboto Mono-20" )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(user-init-file "/home/balaz/.config/emacs/init.el" t))
+ '(custom-safe-themes
+   '("00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- user/.local/bin/toggle_touchpad.sh
+++ user/.local/bin/toggle_touchpad.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-device="SynPS/2 Synaptics TouchPad"
-enabled=$(xinput list-props "SynPS/2 Synaptics TouchPad" | grep -o -P ".*Device Enabled.*\K.")
+# xinput list
+# device="SynPS/2 Synaptics TouchPad"
+device="10"
+enabled=$(xinput list-props "$device" | grep -o -P ".*Device Enabled.*\K.")
 
 [[ "$enabled" == "1" ]] && xinput disable "$device" || xinput enable "$device"
 
