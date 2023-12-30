9e64fbd78954c0ac93254ec54cb3ffde7e339840
Andrej Balaz <andrejbalaz001@gmail.com>
2023-12-30 19:55:37 +0100

    added autocompletion

--- user/.config/nvim/lua/plugins/lsp.lua
+++ user/.config/nvim/lua/plugins/lsp.lua
@@ -15,9 +15,11 @@ return {{
     end
 }, {
     'neovim/nvim-lspconfig',
+    dependencies = 'hrsh7th/cmp-nvim-lsp',
     config = function()
         local lspconfig = require('lspconfig')
-        lspconfig.lua_ls.setup({})
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        lspconfig.lua_ls.setup({capabilities = capabilities})
 
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {desc = 'Go to definition'})
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, {desc = 'Go to references'})
---
+++ user/.config/nvim/lua/plugins/nvimcmp.lua
@@ -0,0 +1,34 @@
+return {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-buffer',
+        'dcampos/cmp-snippy',
+        'dcampos/nvim-snippy',
+    },
+    config = function()
+        local cmp = require('cmp')
+        local snippy = require('snippy')
+        snippy.setup({})
+
+        cmp.setup({
+            snippet = {
+                expand = function(args) snippy.expand_snippet(args.body) end
+            },
+            mapping = cmp.mapping.preset.insert({
+                -- ['<Tab>'] = cmp.mapping.complete(),
+                ['<Tab>'] = cmp.mapping.select_next_item(),
+                ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+                ['<C-j>'] = cmp.mapping.scroll_docs(5),
+                ['<C-k>'] = cmp.mapping.scroll_docs(-5),
+                ['<CR>'] = cmp.mapping.confirm({select = true}),
+            }),
+            sources = {
+                {name = 'nvim_lsp'},
+                {name = 'path'},
+                {name = 'buffer'},
+            }
+        })
+    end
+}
