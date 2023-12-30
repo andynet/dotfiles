2c61d6926b3f3fd5f9afab8145879e6925d3afb4
Andrej Balaz <andrejbalaz001@gmail.com>
2023-12-30 02:31:55 +0100

    added gitsigns improvements

--- user/.config/nvim/lua/plugins/gitsigns.lua
+++ user/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,9 +1,10 @@
 return {
     'lewis6991/gitsigns.nvim',
     config = function ()
-        require('gitsigns').setup({})
+        require('gitsigns').setup()
+	vim.keymap.set('n', '<leader>gb', ":Gitsigns blame_line<CR>", {desc = '[gb] Git blame'})
+	vim.keymap.set('n', '<leader>gn', ":Gitsigns next_hunk<CR>", {desc = '[gn] Git next hunk'})
+	vim.keymap.set('n', '<leader>gv', ":Gitsigns preview_hunk<CR>", {desc = '[gv] Git view hunk'})
+	vim.keymap.set('n', '<leader>gu', ":Gitsigns reset_hunk<CR>", {desc = '[gu] Git undo'})
     end
--- :Gitsigns toggle_current_line_blame
--- :Gitsigns next_hunk
--- :Gitsigns reset_hunk
 }
