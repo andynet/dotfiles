c1c295833f3b1e6acaa6ada6ee770d2c6da6cbfb
Andrej Balaz <andrejbalaz001@gmail.com>
2023-12-30 10:36:59 +0100

    added options, keymaps and autocommands

--- user/.config/nvim/init.lua
+++ user/.config/nvim/init.lua
@@ -10,3 +10,29 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
+
+vim.opt.number = true
+vim.opt.mouse = ""
+vim.opt.scrolloff = 5
+vim.opt.clipboard:append('unnamedplus')
+vim.opt.listchars = "tab:> ,trail:."
+vim.opt.list = true
+vim.opt.colorcolumn = "80,120"
+vim.opt.wrap = false
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.keymap.set({'i', 'n'}, '<C-s>', '<ESC>:w<CR>')
+vim.keymap.set({'n', 'v'}, '<Space>', '<C-f>', {})
+vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', {})
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', 'x', '"_x')
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('YankHighlight', {clear = true}),
+    pattern = '*',
+    callback = function() vim.highlight.on_yank() end,
+})
+
