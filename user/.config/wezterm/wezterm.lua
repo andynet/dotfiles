2060e51188d85314c3bccb65cc8dd114134229f7
Andrej Balaz <andrejbalaz001@gmail.com>
2024-07-28 16:36:09 +0200

    changed to wezterm and cleaned dotfiles

---
+++ user/.config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'GruvboxDark'
+config.font = wezterm.font 'RobotoMono Nerd Font'
+config.font_size = 18.0
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = { left = 2, right = 2, top = 2, bottom = 2 }
+config.default_prog = { '/usr/bin/fish', '-l' }
+
+return config
